2655effe290d9ef2fba087bef2a4d95716a09cd0
Mayank Singhai <ushainformatique@gmail.com>
2019-07-22 16:50:11 +0530

    Updates

---
+++ precache-manifest.ae405ab07e6655836b1b405454c5cc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b812c22713bcb3b75e5e1784bea7b10c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "907d234b2f305f6db827",
+    "url": "/monsters-rolodex/static/css/main.fb715563.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/monsters-rolodex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "907d234b2f305f6db827",
+    "url": "/monsters-rolodex/static/js/main.756c9bb0.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
